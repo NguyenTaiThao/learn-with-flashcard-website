a06e5c2dfe2c32cab259eb626a4e4850659e1953
Thao Nguyen <thaogreenhouses@gmail.com>
2020-11-01 19:15:41 +0700

    them modal dang ky

--- SOURCE/FRONTEND/src/components/Header.js
+++ SOURCE/FRONTEND/src/components/Header.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import "@styles/Header.css"
 import { Col, Row, Button } from 'react-bootstrap'
 import { Modal, Form, Input, Checkbox } from 'antd'
-import { UserOutlined, LockOutlined, FacebookOutlined, GoogleOutlined } from '@ant-design/icons'
+import { UserOutlined, LockOutlined, FacebookOutlined, GoogleOutlined, MailOutlined } from '@ant-design/icons'
 
 
 export default class Header extends Component {
@@ -75,8 +75,13 @@ export default class Header extends Component {
                         className="w-100 mb-4"
                     >
                         <Row className="align-items-center justify-content-center">
-                            <FacebookOutlined className="mr-1" />
-                            <b>Login with Facebook</b>
+                            <FacebookOutlined
+                                className="mr-1"
+                                style={{
+                                    fontSize: "25px",
+                                }}
+                            />
+                            <b>Đăng nhập với Facebook</b>
                         </Row>
                     </Button>
 
@@ -85,13 +90,18 @@ export default class Header extends Component {
                         className="w-100 mb-4"
                     >
                         <Row className="align-items-center justify-content-center">
-                            <GoogleOutlined className="mr-1" />
-                            <b>Login with Google</b>
+                            <GoogleOutlined
+                                className="mr-1"
+                                style={{
+                                    fontSize: "25px",
+                                }}
+                            />
+                            <b>Đăng nhập với Google</b>
                         </Row>
                     </Button>
 
                     <div className="text-center mb-4 or-option">
-                        <b>HOẶC</b>
+                        <b>HOẶC EMAIL</b>
                     </div>
 
                     <Form
@@ -113,7 +123,7 @@ export default class Header extends Component {
                         >
                             <Input
                                 prefix={<UserOutlined className="site-form-item-icon" />}
-                                placeholder="Username"
+                                placeholder="Tên người dùng"
                                 className="login-input"
                             />
                         </Form.Item>
@@ -129,7 +139,7 @@ export default class Header extends Component {
                             <Input
                                 prefix={<LockOutlined className="site-form-item-icon" />}
                                 type="password"
-                                placeholder="Password"
+                                placeholder="Mật khẩu"
                                 className="login-input"
                             />
                         </Form.Item>
@@ -159,9 +169,118 @@ export default class Header extends Component {
                     onOk={this.handleOk}
                     onCancel={() => this.handleShow("registerModal", false)}
                 >
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
-                    <p>Some contents...</p>
+                    <Row className="mb-4">
+                        <Col className="px-0 pr-md-1">
+                            <Button
+                                variant="outline-primary"
+                                className="w-100"
+                            >
+                                <Row className="align-items-center justify-content-center">
+                                    <FacebookOutlined
+                                        className="mr-1"
+                                        style={{
+                                            fontSize: "25px",
+                                        }}
+                                    />
+                                    <b>Tiếp tục với Facebook</b>
+                                </Row>
+                            </Button>
+                        </Col>
+
+                        <Col className="px-0 ml-md-1">
+                            <Button
+                                variant="outline-danger"
+                                className="w-100"
+                            >
+                                <Row className="align-items-center justify-content-center">
+                                    <GoogleOutlined
+                                        className="mr-1"
+                                        style={{
+                                            fontSize: "25px",
+                                        }}
+                                    />
+                                    <b>Tiếp tục với Google</b>
+                                </Row>
+                            </Button>
+                        </Col>
+                    </Row>
+
+                    <Row className="mb-4">
+                        <Col className="px-0">
+                            <hr className="form-splitter"></hr>
+                        </Col>
+                        <Col className="text-center px-0 or-option"><b>HOẶC EMAIL</b></Col>
+                        <Col className="px-0">
+                            <hr className="form-splitter"></hr>
+                        </Col>
+                    </Row>
+
+                    <Form
+                        name="normal_login"
+                        className="login-form"
+                        initialValues={{
+                            remember: true,
+                        }}
+                    // onFinish={onFinish}
+                    >
+                        <Form.Item
+                            name="username"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Vui lòng nhập username!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<UserOutlined className="site-form-item-icon" />}
+                                placeholder="Tên người dùng"
+                                className="login-input"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="email"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Vui lòng nhập email!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<MailOutlined className="site-form-item-icon" />}
+                                placeholder="Email"
+                                className="login-input"
+                            />
+                        </Form.Item>
+
+                        <Form.Item
+                            name="password"
+                            rules={[
+                                {
+                                    required: true,
+                                    message: 'Vui lòng nhập mật khẩu!',
+                                },
+                            ]}
+                        >
+                            <Input
+                                prefix={<LockOutlined className="site-form-item-icon" />}
+                                type="password"
+                                placeholder="Mật khẩu"
+                                className="login-input"
+                            />
+                        </Form.Item>
+                        <Form.Item
+                            name="remember"
+                            valuePropName="checked"
+                            // noStyle
+                            className="text-left"
+                        >
+                            <Checkbox>Tôi chấp nhận <span className="text-info">Điều khoản dịch vụ</span> và <span className="text-info">chính sách quyền riêng tư</span></Checkbox>
+                        </Form.Item>
+                    </Form>
+
                 </Modal>
             </>
         )
