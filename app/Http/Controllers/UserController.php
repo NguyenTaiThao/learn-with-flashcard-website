fc9de0389d7554514dd041d7e28a890e08579908
Trung <ductrung.2112xx@gmail.com>
2020-12-23 09:32:23 +0700

    fix login

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use App\Models\Folder;
 use App\Models\User;
 use App\Models\Set;
 use App\Models\Card;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -22,7 +24,7 @@ class UserController extends Controller
         [
             'email.required' => 'Không được để trống email',
             'password.required' => 'Không được để trống password',
-            'password.required' => 'Không được để trống tên',
+            'name.required' => 'Không được để trống tên',
             'password.min' => 'Mật khẩu phải dài ít nhất 8 ký tự',
             're_password.min' => 'Mật khẩu nhập lại phải dài ít nhất 8 ký tự'
         ]);
@@ -45,6 +47,32 @@ class UserController extends Controller
 
     public function login(Request $request)
     {
-        
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|min:8',
+        ],
+        [
+            'email.required' => 'Không được để trống email',
+            'password.required' => 'Không được để trống password',
+            'password.min' => 'Mật khẩu phải dài ít nhất 8 ký tự'
+        ]);
+        if(Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+            $remember_token = Hash::make(Str::random().time());
+            $user = User::where('email', $request->email)->first();
+            $user->remember_token = $remember_token;
+            $user->save();
+            return [
+                'status' => 1,
+                'code' => 1,
+                'msg' => 'Login successfully',
+                'data' => $user
+            ];
+        }else{
+            return [
+                'status' => 0,
+                'code' => 12,
+                'msg' => 'Email or password is incorrect'
+            ];
+        }
     }
 }
