d72ed536bb62efb3b411a6200aea598d66767cc0
Thao Nguyen <thaogreenhouses@gmail.com>
2020-12-27 10:24:00 +0700

    add createFolder

--- SOURCE/FRONTEND/src/screens/CreateFolder.js
+++ SOURCE/FRONTEND/src/screens/CreateFolder.js
@@ -3,17 +3,31 @@ import { TextField } from '@material-ui/core/'
 import { Row, Col } from "react-bootstrap"
 import { Button } from "antd"
 import "@styles/CreateFolder.css"
+import {requestCreateFolder} from "@constants/Api"
+import NotifyContext from "@context/NotifyContext"
+import reactotron from "reactotron-react-js"
+
+const defaultState = {
+    open: false,
+    form: {
+        id: 0,
+        name: "",
+        discription: ""
+    },
+    loading:false
+}
 
 class CreateFolder extends Component {
 
     constructor(props) {
         super(props)
-        this.state = {
-            open: false
-        }
+        this.state = {...defaultState}
     }
 
+    static contextType = NotifyContext
+
     render() {
+        const { form } = this.state
         return (
             <>
                 <>
@@ -27,6 +41,8 @@ class CreateFolder extends Component {
                                     id="title"
                                     className="w-100 py-3"
                                     label="Tiêu đề"
+                                    value={form.name}
+                                    onChange={(e) => this.handleChange("name", e.target.value)}
                                 />
                             </Row>
                             <Row>
@@ -34,10 +50,19 @@ class CreateFolder extends Component {
                                     id="discription"
                                     className="w-100 py-3"
                                     label="Mô tả"
+                                    value={form.discription}
+                                    onChange={(e) => this.handleChange("discription", e.target.value)}
                                 />
                             </Row>
                             <Row className="mt-4">
-                                <Button type="primary" className="w-100">Tạo thư mục</Button>
+                                <Button
+                                    type="primary"
+                                    className="w-100"
+                                    onClick={() => this.handleCreate()}
+                                    loading={this.state.loading}
+                                >
+                                    Tạo thư mục
+                                </Button>
                             </Row>
 
                         </Col>
@@ -45,6 +70,31 @@ class CreateFolder extends Component {
                 </>
             </>
         )
+    }
+
+    handleChange(field, value) {
+        this.setState({
+            form: {
+                ...this.state.form,
+                [field]: value
+            }
+        })
+    }
+
+    async handleCreate(){
+        try{
+            this.setState({loading:true})
+            const res = await requestCreateFolder({...this.state.form})
+            this.setState({
+                loading:false,
+                form: {...defaultState.form}
+            })
+            this.context("success", "Thành công", "Thêm mới thư mục thành công.")
+        }catch(e){
+            reactotron.log("them folder", e)
+            this.context("error", "Thất bại", e.msg)
+            this.setState({loading:false})
+        }
     }
 }
 
