e289173f6ae3ab1f69928ea6937cf93d5c2264fa
Trung <ductrung.2112xx@gmail.com>
2020-12-28 23:40:26 +0700

    fix API getWeather

--- app/Http/Controllers/GetWeatherController.php
+++ app/Http/Controllers/GetWeatherController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 
 class GetWeatherController extends Controller
@@ -10,29 +11,38 @@ class GetWeatherController extends Controller
 
     public function getWeather()
     {
-        $city_ID = "1581129";
-        $google_api_url = "https://api.openweathermap.org/data/2.5/weather?id=" . $city_ID . "&lang=vi&units=metric&APPID=" . $this->api_key;
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_URL, $google_api_url);
-        curl_setopt($ch, CURLOPT_VERBOSE, 1);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        try{
+            $city_ID = "1581129";
+            $google_api_url = "https://api.openweathermap.org/data/2.5/weather?id=" . $city_ID . "&lang=vi&units=metric&APPID=" . $this->api_key;
+            $ch = curl_init();
+            curl_setopt($ch, CURLOPT_HEADER, 0);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_URL, $google_api_url);
+            curl_setopt($ch, CURLOPT_VERBOSE, 1);
+            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 
-        $response = curl_exec($ch);
-        curl_close($ch);
-        $data = json_decode($response);
-        $current_time = time();
-        $returnData = [
-            'city_name' => $data->name,
-            'current_time' => $current_time,
-            'description' => ucwords($data->weather[0]->description),
-            'icon' => "http://openweathermap.org/img/w/" . $data->weather[0]->icon . ".png",
-            'temp_max' => $data->main->temp_max,
-            'temp_min' => $data->main->temp_min,
-            'humidity(%)' => $data->main->humidity,
-            'wind(km/h)' => $data->wind->speed,
-        ];
-        return ($returnData);
+            $response = curl_exec($ch);
+            curl_close($ch);
+            $data = json_decode($response);
+            $current_time = time();
+            $weather_info = [
+                'city_name' => $data->name,
+                'current_time' => date("F j, Y, g:i a", $current_time),
+                'description' => ucwords($data->weather[0]->description),
+                'icon' => "http://openweathermap.org/img/w/" . $data->weather[0]->icon . ".png",
+                'temp_max' => $data->main->temp_max,
+                'temp_min' => $data->main->temp_min,
+                'humidity(%)' => $data->main->humidity,
+                'wind(km/h)' => $data->wind->speed,
+            ];
+            $returnData = [
+                'status' => 1,
+                'msg' => 'Get weather information successfully',
+                'data' => $weather_info
+            ];
+            return response()->json($returnData, 200);
+        }catch(Exception $e){
+            return $this->internalServerError($e);
+        }
     }
 }
