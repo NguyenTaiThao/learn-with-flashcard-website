ed89eed2f6b566cf765acbfae3c0562e408cda16
dungbk10t <38665090+dungbk10t@users.noreply.github.com>
2021-01-19 22:54:27 +0700

    Format code Page2

--- SOURCE/FRONTEND/src/screens/Home.js
+++ SOURCE/FRONTEND/src/screens/Home.js
@@ -135,119 +135,119 @@ class HomeScreen extends Component {
             </Row>
         )
     }
-renderPage2() {
-    return (
-        <Row className="page-2 w-100">
-            <Col xs={12} className="my-5 py-5">
-                <Row className="title my-5">
-                    <p className="text-center w-100"><em>90%</em>người dùng Quizlet cho biết họ đã cải thiện được điểm số</p>
-                </Row>
-            </Col>
+    renderPage2() {
+        return (
+            <Row className="page-2 w-100">
+                <Col xs={12} className="my-5 py-5">
+                    <Row className="title my-5">
+                        <p className="text-center w-100"><em>90%</em>người dùng Quizlet cho biết họ đã cải thiện được điểm số</p>
+                    </Row>
+                </Col>
 
-            <Col xs={12} className="px-5 home-element">
-                <Row>
-                    <Col md={6} className="text">
-                        <p className="title">Bạn chỉ cần động não, còn mọi thứ khác đã có chúng tôi lo.</p>
-                        <p className="content">Từ các thẻ ghi nhớ giúp bạn học tiếng Anh,
-                        đến các trò chơi giúp học lịch sử dễ dàng,
-                        bạn có thể sử dụng nhiều loại công cụ để chinh phục mọi thử thách.
-                        </p>
-                    </Col>
-                    <Col md={6} className="picture">
-                        <img src={require("@assets/effective.png")} className="w-100" alt="anh minh hoa"></img>
-                    </Col>
-                </Row>
-            </Col>
+                <Col xs={12} className="px-5 home-element">
+                    <Row>
+                        <Col md={6} className="text">
+                            <p className="title">Bạn chỉ cần động não, còn mọi thứ khác đã có chúng tôi lo.</p>
+                            <p className="content">Từ các thẻ ghi nhớ giúp bạn học tiếng Anh,
+                            đến các trò chơi giúp học lịch sử dễ dàng,
+                            bạn có thể sử dụng nhiều loại công cụ để chinh phục mọi thử thách.
+                            </p>
+                        </Col>
+                        <Col md={6} className="picture">
+                            <img src={require("@assets/effective.png")} className="w-100" alt="anh minh hoa"></img>
+                        </Col>
+                    </Row>
+                </Col>
 
-            <Col xs={12} className="mt-1 home-element">
-                <Row>
-                    <Col md={6} className="picture">
-                        <img src={require("@assets/success.png")} className="w-100" alt="anh minh hoa"></img>
-                    </Col>
-                    <Col md={6} className="text">
-                        <p className="title">Thành công tiếp theo của bạn đang ở rất gần rồi.</p>
-                        <p className="content">Mỗi kiến thức mới bạn học là một thành tích.
-                        Quizlet chia nhỏ các chủ đề và môn học để bạn tiến bộ từng ngày.
-                        </p>
-                    </Col>
-                </Row>
-            </Col>
+                <Col xs={12} className="mt-1 home-element">
+                    <Row>
+                        <Col md={6} className="picture">
+                            <img src={require("@assets/success.png")} className="w-100" alt="anh minh hoa"></img>
+                        </Col>
+                        <Col md={6} className="text">
+                            <p className="title">Thành công tiếp theo của bạn đang ở rất gần rồi.</p>
+                            <p className="content">Mỗi kiến thức mới bạn học là một thành tích.
+                            Quizlet chia nhỏ các chủ đề và môn học để bạn tiến bộ từng ngày.
+                            </p>
+                        </Col>
+                    </Row>
+                </Col>
 
-            <Col xs={12} className="mb-5 pb-5 home-element">
-                <Row>
-                    <Col md={6} className="text">
-                        <p className="title">Đừng nản lòng. Cùng nỗ lực nào.</p>
-                        <p className="content">Khi một bài học nhỏ nhất cũng mang lại cảm giác chiến thắng,
-                        bạn sẽ có thêm động lực tiếp bước.
-                        </p>
-                    </Col>
-                    <Col md={6} className="picture">
-                        <img src={require("@assets/try.png")} className="w-100" alt="anh minh hoa"></img>
-                    </Col>
-                </Row>
-            </Col>
+                <Col xs={12} className="mb-5 pb-5 home-element">
+                    <Row>
+                        <Col md={6} className="text">
+                            <p className="title">Đừng nản lòng. Cùng nỗ lực nào.</p>
+                            <p className="content">Khi một bài học nhỏ nhất cũng mang lại cảm giác chiến thắng,
+                            bạn sẽ có thêm động lực tiếp bước.
+                            </p>
+                        </Col>
+                        <Col md={6} className="picture">
+                            <img src={require("@assets/try.png")} className="w-100" alt="anh minh hoa"></img>
+                        </Col>
+                    </Row>
+                </Col>
 
-            <Col xs={12} className="home-element my-3">
-                <CarouselBootstrap
-                    indicators={false}
-                    controls={false}
-                    keyboard={false}
-                >
-                    <CarouselBootstrap.Item interval={5000}>
-                        <h3 className="user-review">Tôi phát hiện ra Quizlet hồi mới 12 tuổi.
-                        Nó đã giúp tôi cải thiện điểm số rất nhiều.
-                        Cảm ơn Quizlet đã giúp cho việc học trở nên vui vẻ và hiệu quả!
-                        </h3>
-                        <p className="user-info">-Nguyễn Tài Thao, 21 tuổi</p>
-                    </CarouselBootstrap.Item>
-                    <CarouselBootstrap.Item interval={5000}>
-                        <h3 className="user-review">Quizlet là một phương pháp học tuyệt vời.
-                        Tôi đã giới thiệu nó cho bạn bè và tất cả chúng tôi đang tiến bộ.
-                        Bất cứ khi nào nghĩ về Quizlet,
-                        tôi lại thấy niềm vui khi học trong vài phút thay vì nhiều giờ.
-                        </h3>
-                        <p className="user-info">-Trần Đức Trung, 22 tuổi</p>
-                    </CarouselBootstrap.Item>
-                    <CarouselBootstrap.Item interval={5000}>
-                        <h3 className="user-review">Trước đây, khi nói đến việc học, tôi dở vô cùng.
-                        Bây giờ tôi đang ở một trường đại học mới và họ giới thiệu tôi với Quizlet.
-                        Tôi không còn căng thẳng mỗi khi học và làm bài tập nữa. CẢM ƠN QUIZLET!!!
-                        </h3>
-                        <p className="user-info">-Phạm Tuấn Dũng, 18 tuổi</p>
-                    </CarouselBootstrap.Item>
-                </CarouselBootstrap>
-            </Col>
+                <Col xs={12} className="home-element my-3">
+                    <CarouselBootstrap
+                        indicators={false}
+                        controls={false}
+                        keyboard={false}
+                    >
+                        <CarouselBootstrap.Item interval={5000}>
+                            <h3 className="user-review">Tôi phát hiện ra Quizlet hồi mới 12 tuổi.
+                            Nó đã giúp tôi cải thiện điểm số rất nhiều.
+                            Cảm ơn Quizlet đã giúp cho việc học trở nên vui vẻ và hiệu quả!
+                            </h3>
+                            <p className="user-info">-Nguyễn Tài Thao, 21 tuổi</p>
+                        </CarouselBootstrap.Item>
+                        <CarouselBootstrap.Item interval={5000}>
+                            <h3 className="user-review">Quizlet là một phương pháp học tuyệt vời.
+                            Tôi đã giới thiệu nó cho bạn bè và tất cả chúng tôi đang tiến bộ.
+                            Bất cứ khi nào nghĩ về Quizlet,
+                            tôi lại thấy niềm vui khi học trong vài phút thay vì nhiều giờ.
+                            </h3>
+                            <p className="user-info">-Trần Đức Trung, 22 tuổi</p>
+                        </CarouselBootstrap.Item>
+                        <CarouselBootstrap.Item interval={5000}>
+                            <h3 className="user-review">Trước đây, khi nói đến việc học, tôi dở vô cùng.
+                            Bây giờ tôi đang ở một trường đại học mới và họ giới thiệu tôi với Quizlet.
+                            Tôi không còn căng thẳng mỗi khi học và làm bài tập nữa. CẢM ƠN QUIZLET!!!
+                            </h3>
+                            <p className="user-info">-Phạm Tuấn Dũng, 18 tuổi</p>
+                        </CarouselBootstrap.Item>
+                    </CarouselBootstrap>
+                </Col>
 
-            <Col xs={12} className="text-center confirm-div">
-                <h3 className="question">Bạn đã sẵn sàng cải thiện điểm số?</h3>
-                <Button
-                    variant="info"
-                    className="learn-now-btn"
-                >
-                    Bắt đầu học
-                </Button>
-            </Col>
+                <Col xs={12} className="text-center confirm-div">
+                    <h3 className="question">Bạn đã sẵn sàng cải thiện điểm số?</h3>
+                    <Button
+                        variant="info"
+                        className="learn-now-btn"
+                    >
+                        Bắt đầu học
+                    </Button>
+                </Col>
 
-            <Col xs={12} className="teacher-div">
-                <Row>
-                    <Col md={6} className="left">
-                        <p className="teacher">GIÁO VIÊN</p>
-                        <p className="role">Truyền năng lượng cho học sinh của bạn</p>
-                        <p className="target">Giúp các em tự tin học bất cứ nội dung nào,
-                        dù cho mục tiêu phấn đấu có là gì đi nữa. Sử dụng các học phần miễn phí,
-                        chế độ học và trò chơi trong lớp của Quizlet,
-                        bạn có thể dễ dàng khích lệ học sinh của mình.
-                        </p>
-                        <Link className="link">Mang Quizlet đến lớp của bạn</Link>
-                    </Col>
-                    <Col md={6} className="px-0 right">
-                        <img src={require("@src/assets/teacher.png")} className="w-100" alt="anh-slide" />
-                    </Col>
-                </Row>
-            </Col>
-        </Row >
-    )
-}
+                <Col xs={12} className="teacher-div">
+                    <Row>
+                        <Col md={6} className="left">
+                            <p className="teacher">GIÁO VIÊN</p>
+                            <p className="role">Truyền năng lượng cho học sinh của bạn</p>
+                            <p className="target">Giúp các em tự tin học bất cứ nội dung nào,
+                            dù cho mục tiêu phấn đấu có là gì đi nữa. Sử dụng các học phần miễn phí,
+                            chế độ học và trò chơi trong lớp của Quizlet,
+                            bạn có thể dễ dàng khích lệ học sinh của mình.
+                            </p>
+                            <Link className="link">Mang Quizlet đến lớp của bạn</Link>
+                        </Col>
+                        <Col md={6} className="px-0 right">
+                            <img src={require("@src/assets/teacher.png")} className="w-100" alt="anh-slide" />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row >
+        )
+    }
 renderFooter() {
     return (
         <>
