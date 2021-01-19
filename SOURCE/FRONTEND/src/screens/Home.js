21a1d6b46f343c08d00c00d39acaa5d7c44e0365
dungbk10t <38665090+dungbk10t@users.noreply.github.com>
2021-01-19 22:07:37 +0700

    Create home page

---
+++ SOURCE/FRONTEND/src/screens/Home.js
@@ -0,0 +1,21 @@
+import React, {
+    Component
+} from 'react';
+import {
+    Row,
+    Col,
+    Button,
+} from 'react-bootstrap'
+import CarouselBootstrap from 'react-bootstrap/Carousel'
+import "@styles/Home.css"
+import { Link, Redirect } from "react-router-dom"
+import {ROUTER} from "@constants/Constant"
+import { Carousel, Select } from "antd";
+import Divider from '@material-ui/core/Divider';
+import {
+    YoutubeOutlined,
+    FacebookFilled,
+    TwitterOutlined,
+    InstagramOutlined
+} from '@ant-design/icons';
+import Cookie from "js-cookie"
